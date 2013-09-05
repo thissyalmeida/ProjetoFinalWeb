2befed9d9f52dc0fa194931dd55a67083b3b1bf9
thissyalmeida <thissy.almeida@gmail.com>
2013-09-05 16:46:38 -0300

    Criação do Script do banco de dados

---
+++ tabelas.sql
@@ -0,0 +1,16 @@
+/* Deleta as tabelas, caso elas já existam */
+DROP TABLE IF EXISTS Yoopielogin;
+
+/* Cria as tabelas */
+CREATE TABLE Yoopielogin (
+	id TINYINT primary key auto_increment,
+	nome VARCHAR(32) NOT NULL,
+	email VARCHAR(32) NOT NULL,
+	usuario VARCHAR(16) NOT NULL,
+	senha VARCHAR(32) NOT NULL,
+	acesso TINYINT NOT NULL
+	);
+
+/* Insere valores padroes */
+INSERT INTO smartlogin (nome,email,usuario,senha,acesso) VALUES ('Thissiany Beatriz Almeida','thissy.almeida@gmail.com','thissyAlmeida','123456','1');
+
